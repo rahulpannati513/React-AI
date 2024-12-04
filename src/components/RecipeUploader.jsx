c65164263c22d2357604338a6e57ba50ce1086d0
rahul <rahul@192.168.1.6>
2024-12-04 16:18:40 +0530

    modified recipe

--- src/components/RecipeUploader.jsx
+++ src/components/RecipeUploader.jsx
@@ -115,15 +115,7 @@ const RecipeUploader = () => {
 
         {/* Error Message */}
         {error && <p className="text-red-600 text-center mt-4">{error}</p>}
-
-        <div>
-          <h1>{recipe?.title}</h1>
-          <p>{recipe?.ingredients}</p>
-          <p>{recipe?.instructions}</p>
-        </div>
       </div>
-
-      {/* Recipe Content Section */}
     </div>
   );
 };
